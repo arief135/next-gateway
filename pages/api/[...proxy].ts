f9f5f0926fef1073de8a0213f4d40ca9f398d45a
Arief Pratama <ariefpratama135@gmail.com>
2023-04-21 14:45:41 +0700

    reorg proxy service

--- pages/api/[...proxy].ts
+++ pages/api/[...proxy].ts
@@ -1,5 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import { ProxyService } from '@/lib/ProxyService'
+import { ProxyService, QueryString } from '@/src/services/proxy.service'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 
@@ -10,11 +10,8 @@ export default async function handler(
 
     const query = req.query.proxy as string[]
 
-    console.log(query)
-
     if (query.length < 2) {
-        res.status(400).write('No system id specified')
-        res.end()
+        res.status(400).send('No system id specified')
         return
     }
 
@@ -22,55 +19,11 @@ export default async function handler(
 
     try {
         const proxyService = new ProxyService(sId)
-
-        const requestUrl = query.slice(2).join('/')
-        const destUrl = proxyService.mapUrl(requestUrl)
-
-        console.log(destUrl)
+        await proxyService.send(req, res)
     } catch (error: any) {
-        res.status(400).write(error.message)
-        res.end()
+        res.status(400).send(error.message)
         return
     }
 
-
     res.end()
-
-    // let destination = process.env.SAP_HOST
-
-    // if (Array.isArray(query)) {
-    //     destination = destination + '/' + query.join('/')
-    // } else {
-    //     destination = destination + '/' + query
-    // }
-
-    // const queryString = { ...req.query }
-    // delete queryString.sap
-
-    // const headers = { ...req.headers }
-    // delete headers.host
-
-    // const requestProp = {
-    //     url: destination,
-    //     headers: headers,
-    //     method: req.method,
-    //     params: queryString,
-    // }
-
-    // try {
-    //     const axiosResponse = await axios.request(requestProp)
-
-    //     Object.keys(axiosResponse.headers).forEach((k) => {
-    //         res.setHeader(k, axiosResponse.headers[k])
-    //     })
-
-    //     res.status(axiosResponse.status).send(axiosResponse.data)
-    // } catch (error: any) {
-    //     Object.keys(error.response.headers).forEach((k) => {
-    //         res.setHeader(k, error.response.headers[k])
-    //     })
-
-    //     res.status(error.response.status)
-    //         .send(error.response.data)
-    // }
 }
---
+++ src/services/proxy.service.ts
@@ -0,0 +1,99 @@
+import axios from "axios"
+import { NextApiRequest, NextApiResponse } from "next"
+
+export type QueryString = { [key: string]: string }
+
+type BasicAuth = {
+    user: string,
+    password: string
+}
+
+type ProxySystem = {
+    backend: string,
+    baseUrl: string,
+    queryString: QueryString,
+    auth: BasicAuth
+}
+
+const systemMap: { [sId: string]: ProxySystem } = {
+    'adhi-odata-100': {
+        backend: 'S/4HANA',
+        baseUrl: 'https://vhptrds4ci.sap.adhi.co.id:44300/sap/opu/odata/sap',
+        queryString: {
+            'sap-client': '100',
+            'sap-language': 'EN'
+        },
+        auth: {
+            user: process.env.SAP_USER as string,
+            password: process.env.SAP_PASSWORD as string
+        }
+    }
+}
+
+
+
+export class ProxyService {
+
+    private sId: string
+    private sysDetails: ProxySystem
+
+    constructor(sId: string) {
+        if (sId in systemMap) {
+            this.sId = sId
+            this.sysDetails = systemMap[this.sId]
+        } else {
+            throw new Error(`System ID ${sId} not found`);
+        }
+    }
+
+    public mapUrl(path: string, queries: QueryString) {
+        const url = new URL(this.sysDetails.baseUrl + '/' + path)
+
+        for (const key in this.sysDetails.queryString) {
+            url.searchParams.set(key, this.sysDetails.queryString[key])
+        }
+
+        for (const key in queries) {
+            url.searchParams.set(key, queries[key])
+        }
+
+        return url.toString()
+    }
+
+    public async send(req: NextApiRequest, res: NextApiResponse) {
+        const path = req.query.proxy as string[]
+        const requestUrl = path.slice(2).join('/')
+        const queryString = { ...req.query } as QueryString
+        delete queryString.proxy
+
+        const headers = { ...req.headers }
+        delete headers.host
+
+        const requestProp = {
+            url: this.mapUrl(requestUrl, queryString),
+            headers: headers,
+            method: req.method,
+        }
+
+        try {
+            const axiosResponse = await axios.request(requestProp)
+
+            Object.keys(axiosResponse.headers).forEach((k) => {
+                res.setHeader(k, axiosResponse.headers[k])
+            })
+
+            res.status(axiosResponse.status).send(axiosResponse.data)
+        } catch (error: any) {
+
+            if (error.response !== undefined) {
+                Object.keys(error.response.headers).forEach((k) => {
+                    res.setHeader(k, error.response.headers[k])
+                })
+
+                res.status(error.response.status).send(error.response.data)
+            } else {
+                res.status(500).send(error.message)
+            }
+        }
+    }
+}

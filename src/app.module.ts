205df47c141b2472f3f73d709136e217be932f45
Arief Pratama <ariefpratama135@gmail.com>
2023-10-08 17:44:28 +0700

    Squashed commit of the following:
    commit b4bf866919a1964e06737ebc14032a3a6784809b
    Author: Arief Pratama <ariefpratama135@gmail.com>
    Date:   Sun Oct 8 17:41:15 2023 +0700
    
        add unit test
    
    commit 44339e4be5947736e2650d8873c1c0162f5e92fe
    Author: Arief Pratama <ariefpratama135@gmail.com>
    Date:   Sun Oct 8 17:36:11 2023 +0700
    
        integrate jwt tokens
    
    commit 925f988d9fd7b8975f8d84359e34b9a76614f741
    Author: Arief Pratama <ariefpratama135@gmail.com>
    Date:   Sun Oct 8 17:16:12 2023 +0700
    
        add authentication with jwt
    
    commit 83b87c31fade83fb187d2a5b50f6b73ecdb7aa70
    Author: Arief  Pratama <ariefpratama135@gmail.com>
    Date:   Fri Aug 4 23:51:19 2023 +0700
    
        update proxy entity
    
    commit b4fff29cfe0eac294ceb0263cc91e519bae973a2
    Author: Arief  Pratama <ariefpratama135@gmail.com>
    Date:   Thu Aug 3 23:27:28 2023 +0700
    
        scaffold proxy api result
    
    commit 44edb54dd55b520121f1d48a66388d51ff112b49
    Author: Arief  Pratama <ariefpratama135@gmail.com>
    Date:   Mon Jul 31 16:46:25 2023 +0700
    
        add entity class

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProxiesModule } from './proxies/proxies.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [ProxiesModule],
+  imports: [ProxiesModule, AuthModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
